bd3e3169712a700315f4a1fef2f440d08479620a
AvindaShamal <avindashamal@gmail.com>
2026-03-02 01:40:22 +0530

    Add batch inference functionality

---
+++ pipelines/batch_inference_pipeline.py
@@ -0,0 +1,40 @@
+import mlflow.pyfunc as pyfunc
+import yaml
+import pandas as pd
+
+from src.s3_utils import upload_to_s3
+
+
+def load_model_from_registry(
+    model_name: str, model_version: str = "Staging"
+) -> pyfunc.PyFuncModel:
+    model_uri = f"models:/{model_name}/{model_version}"
+    model = pyfunc.load_model(model_uri)
+    return model
+
+
+def run_batch_inference(config_path: str) -> None:
+    # Load configuration
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+
+    # Load model from MLflow registry
+    model = load_model_from_registry(
+        model_name=config["model"]["registry_name"],
+    )
+
+    # Load new data for inference
+    inference_data_path = config["data"]["inference_data_path"]
+    df = pd.read_csv(inference_data_path, parse_dates=["Date"])
+
+    predictions = model.predict(df)
+    df["Forecasted_Demand"] = predictions
+
+    # Save predictions to CSV
+    output_path = config["output"]["predictions_output_path"]
+    df.to_csv(output_path, index=False)
+    upload_to_s3(
+        output_path, config["aws"]["s3_bucket"], "predictions/batch_predictions.csv"
+    )
+
+    print(f"Batch inference completed. Predictions saved to {output_path}")
