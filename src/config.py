7f9769aa471dec57f071c0abd77f5ea0966ca31f
AvindaShamal <avindashamal@gmail.com>
2026-03-02 13:52:59 +0530

    updated parameter list

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ FEATURE_EXCLUDE = ["Date", TARGET]
 
 PARAM_GRID = {
     "max_depth": [4, 6, 8],
-    "learning_rate": [0.01, 0.02],
+    "learning_rate": [0.01],
     "n_estimators": [300, 500, 700],
 }
 
